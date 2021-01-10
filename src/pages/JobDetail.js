e55ecd2976ec7b35058c6e656df658c89af38a49
Su988 <suyash988@gmail.com>
2021-01-10 13:16:06 -0500

    jobDetail page css added ...next icons

--- src/pages/JobDetail.js
+++ src/pages/JobDetail.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, Fragment } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Context } from '../Context';
+import './jobDetail.css';
 
 function JobInfo() {
   const { jobId } = useParams();
@@ -14,23 +15,39 @@ function JobInfo() {
   return (
     <Fragment>
       {selectedJob ? (
-        <div>
+        <div className='jobDetail'>
           <div className='side'>
             <Link to='/'>
-              <p>back to search</p>
+              <p>Back to search</p>
             </Link>
-            <p>HOW TO APPLY</p>
+            <p id='apply'>HOW TO APPLY</p>
             <div
               dangerouslySetInnerHTML={{ __html: selectedJob.how_to_apply }}
+              id='apply-how'
             ></div>
           </div>
           <div className='main'>
-            <h2>{selectedJob.company}</h2>
-            <p>{selectedJob.created_at}</p>
-            <img src={selectedJob.company_logo} alt='' width='50' height='50' />
-            <p>{selectedJob.location}</p>
+            <p id='title'>{selectedJob.title}</p>
+            <p id='type'>{selectedJob.type}</p>
+            <p id='date'>{selectedJob.created_at}</p>
+            <div className='logo-flex'>
+              <div>
+                <img
+                  src={selectedJob.company_logo}
+                  alt=''
+                  width='50'
+                  height='50'
+                  id='logo'
+                />
+              </div>
+              <div>
+                <p id='company'>{selectedJob.company}</p>
+                <p id='location'>{selectedJob.location}</p>
+              </div>
+            </div>
             <div
               dangerouslySetInnerHTML={{ __html: selectedJob.description }}
+              id='description'
             ></div>
           </div>
         </div>
